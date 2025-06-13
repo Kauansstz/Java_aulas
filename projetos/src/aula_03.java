d3cf6fe8719c83c8129324ea48a3cc37cd3ed579
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-06-13 17:31:37 -0300

    Mostrado o Var

---
+++ projetos/src/aula_03.java
@@ -0,0 +1,11 @@
+public class aula_03 {
+    public static void main(String[] args) {
+        var p1 = 2;
+        var p2 = 3;
+        System.out.println(p1);
+        System.out.println(p2);
+
+        p1 = "TEXTO";
+        System.out.println(p1);
+    }
+}
