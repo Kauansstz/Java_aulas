3f84f80e7cd595273ff95eb76f443fa648d16eca
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-06-16 10:05:49 -0300

    Import

---
+++ projetos/src/aula_06.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+import javax.swing.JButton;
+
+public class aula_06 {
+    // import
+    public static void main(String[] args) {
+
+        Date d = new Date();
+        System.out.println(d);
+
+        JButton botao = new JButton();
+        System.out.println(botao);
+    }
+}
