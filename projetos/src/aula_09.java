2e87a5a788eb53eb9a665fcf540fe4fb30145240
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-06-16 11:49:51 -0300

    Objeto vs primitivo

---
+++ projetos/src/aula_09.java
@@ -0,0 +1,11 @@
+public class aula_09 {
+    public static void main(String[] args) {
+        
+        String s = "Texto";
+        s.toUpperCase();
+
+        // Wrappers são a versão objeto dos tipos primitivos;
+        int a =123;
+        System.out.println(a);
+    }
+}
