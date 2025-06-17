2d4677a90c1a5a69167de6ace1ea21ecc7b4edae
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-06-17 17:54:22 -0300

    Conversão de int para str e de str para int

---
+++ projetos/src/aula_12.java
@@ -0,0 +1,11 @@
+public class aula_12 {
+    public static void main(String[] args) {
+        Integer a = 1000;
+        System.out.println(a.toString());
+
+        int b = 20;
+        System.out.println(Integer.toString(b));
+
+        System.out.println((""+ b).length());
+    }
+}
