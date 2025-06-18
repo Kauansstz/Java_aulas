7daf6a29519b40a20c9422662b885228bf729da1
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-06-18 16:33:02 -0300

    Condições

---
+++ projetos/src/aula_14.java
@@ -0,0 +1,13 @@
+public class aula_14 {
+    public static void main(String[] args) {
+        
+        boolean condicao1 = true;
+        boolean condicao2 = 1 > 7;
+
+        System.out.println( condicao1 && condicao2);
+        System.out.println( condicao1 || condicao2);
+        System.out.println( condicao1 ^ condicao2);
+        System.out.println( condicao1 != condicao2);
+        System.out.println( condicao1 == condicao2);
+    }
+}
