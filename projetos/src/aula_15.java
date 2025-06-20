f8b064d265a77bb9206cd8269735900286734669
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-06-20 16:24:11 -0300

    Operador Ternário

---
+++ projetos/src/aula_15.java
@@ -0,0 +1,9 @@
+public class aula_15 {
+    public static void main(String[] args) {
+        
+        double media = 6.5;
+        String resultadoRecuperacao = media >= 7.0 ? "aprovado." : "em recuperação";
+
+        System.out.println("O aluno está " + resultadoRecuperacao);
+    }
+}
