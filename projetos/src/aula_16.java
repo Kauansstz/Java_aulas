6cca8452e602e17c7f197b0d46f81a628da99a87
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-06-24 12:06:39 -0300

    For

---
+++ projetos/src/aula_16.java
@@ -0,0 +1,7 @@
+public class aula_16 {
+    public static void main(String[] args) {
+        for(int contador =1; contador <=10; contador++){
+            System.out.printf("i = %d\n", contador);
+        }
+    }
+}
