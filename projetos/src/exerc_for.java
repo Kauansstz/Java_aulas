624230e18c7fa5bcca5545bdb93e2e698d849713
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-06-26 11:25:59 -0300

    Exercicio de for sem usar valor numerico

---
+++ projetos/src/exerc_for.java
@@ -0,0 +1,13 @@
+
+public class exerc_for {
+
+    public static void main(String[] args) {
+        
+       
+
+        for(String valor = "#"; !valor.equals("######"); valor += "#"){
+            System.out.println(valor);
+            
+        }
+    }
+}
