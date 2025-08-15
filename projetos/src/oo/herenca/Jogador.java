7a5b85dcae252e239aeada239de064b38bb7e36b
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-08-15 18:20:07 -0300

    Herança

--- projetos/src/oo/herenca/Jogador.java
+++ projetos/src/oo/herenca/Jogador.java
@@ -3,7 +3,22 @@ package oo.herenca;
 public class Jogador {
     int x;
     int y;
+    int vida = 100;
 
+    boolean atacar(Jogador oponente){
+        int deltaX = Math.abs(x-oponente.x);
+        int deltaY = Math.abs(x-oponente.y);
+
+        if(deltaX == 0 && deltaY == 1){
+            oponente.vida  -=10;
+            return true;
+        }else if (deltaX == 1 && deltaY == 1) {
+            oponente.vida -= 10;
+            return true;
+        } else{
+            return false;
+        }
+    }
     boolean Andar(Direcao direcao ){
         if(direcao == Direcao.NORTE){
             y++;
--- projetos/src/oo/herenca/Jogo.java
+++ projetos/src/oo/herenca/Jogo.java
@@ -2,12 +2,19 @@ package oo.herenca;
 
 public class Jogo {
     public static void main(String[] args) {
-        Jogador j1 = new Jogador();
-        j1.Andar(Direcao.NORTE);
-        j1.Andar(Direcao.NORTE);
-        j1.Andar(Direcao.NORTE);
-        j1.Andar(Direcao.NORTE);
+        Monstro j1 = new Monstro();
+        j1.x = 10;
+        j1.y = 10;
+        
+        Heroi j2 = new Heroi();
+        j2.x = 10;
+        j2.y = 11;
 
-        System.out.println(j1.y);
+        System.out.println(j1.vida);
+        System.out.println(j2.vida);
+
+        j1.atacar(j2);
+        System.out.println(j1.vida);
+        System.out.println(j2.vida);
     }
 }
