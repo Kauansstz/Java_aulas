087286388d2ec95835b89df718c13af143a12efb
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-08-27 16:09:22 -0300

    For each, Method Reference

---
+++ projetos/src/lambdas/Foreach.java
@@ -0,0 +1,19 @@
+package lambdas;
+
+import java.util.List;
+import java.util.Arrays;
+
+
+public class Foreach {
+    public static void main(String[] args) {
+        List<String> aprovados = Arrays.asList("Maria", "Pedro", "Anthony", "João");
+        System.out.println("Forma tradicional: ");
+        for(String nome: aprovados){
+            System.out.println(nome);
+        }
+        System.out.println("\nLambda #01... ");
+        aprovados.forEach(nome -> System.out.println(nome + "!!!!!!!"));
+        System.out.println("\nMethod Reference...");
+        aprovados.forEach(System.out::println);
+    }
+}
