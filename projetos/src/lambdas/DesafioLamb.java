af0735b15c536dcd27ef3327346e0b2907ae86c3
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-09-03 17:02:34 -0300

    Realizado a tarefa do hadda com o meu código e com o do professor

--- projetos/src/lambdas/DesafioLamb.java
+++ projetos/src/lambdas/DesafioLamb.java
@@ -1,45 +1,71 @@
 package lambdas;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
-public class DesafioLamb extends Produto{
-    private String municipio;
+public class DesafioLamb{
+    // private String municipio;
 
-    public DesafioLamb(String nome, double preco, double desconto, String municipio){
-        super(nome, preco, desconto);
-        this.municipio = municipio;
-    }
-    public static void Arredondar2(Produto... produtos){
-        for(Produto p: produtos){
-            System.out.println(String.format("%.2f", p.getPreco()));
-        }
-        
-    }
-    public static void LojasEMuncipios(DesafioLamb... produtos){
-        Map<String, Double>  taxaMunicipios = new HashMap<>();
-        taxaMunicipios.put("São José", 0.18);
-        taxaMunicipios.put("Palhoça", 0.15);
-        taxaMunicipios.put("Santo Amaro", 0.20);
-        for (DesafioLamb p : produtos){
-            double precoBase = p.getPreco();
-            double taxa = taxaMunicipios.getOrDefault(p.municipio, 0.0);
-            double valorTotal = precoBase * (1 + taxa);
-            System.out.println(p.nome + " | Município: " + p.municipio);
-            System.out.println("Valor com taxa: " + String.format("R$ %.2f", valorTotal));
-            System.out.println();
-        }
-    }
+    // public DesafioLamb(String nome, double preco, double desconto, String municipio){
+    //     super(nome, preco, desconto);
+    //     this.municipio = municipio;
+    // }
     
+    // public static void LojasEMuncipios(DesafioLamb... produtos){
+    //     Map<String, Double>  taxaMunicipios = new HashMap<>();
+    //     taxaMunicipios.put("São José", 0.18);
+    //     taxaMunicipios.put("Palhoça", 0.15);
+    //     taxaMunicipios.put("Santo Amaro", 0.20);
+
+    //     for (DesafioLamb p : produtos){
+    //         double precoBase = p.getPreco();
+    //         if (precoBase >= 2500) {
+    //             final double valorFrete = 100;
+    //             double taxa = taxaMunicipios.getOrDefault(p.municipio, 0.0);
+    //             double valorTotal = precoBase * (1 + taxa) + valorFrete;
+    //             System.out.println("Produto: " + p.nome + " | Município: " + p.municipio + " | Frete: " + valorFrete);
+    //             System.out.println("Valor com taxa: " + String.format("R$ %.2f", valorTotal));
+    //             System.out.println();
+    //         } else{
+    //             final double valorFrete = 50;
+    //             double valorTotal = precoBase + valorFrete;
+    //             System.out.println("Produto: " + p.nome + " | Município: " + p.municipio + " | Frete: " + valorFrete);
+    //             System.out.println("Valor Total: " + String.format("R$ %.2f", valorTotal));
+    //             System.out.println();
+    //         }
+    //     }
+    // }
+    
+    // public static void main(String[] args) {
+    //     DesafioLamb p1 = new DesafioLamb("iPad", 3250.89, 0.13, "São José");
+    //     DesafioLamb p2 = new DesafioLamb("Computador", 8749.99, 0.15, "Palhoça");
+    //     DesafioLamb p3 = new DesafioLamb("Mouse", 350.00, 0.10, "Santo Amaro");
+    //     LojasEMuncipios(p1, p2, p3);
+    // }
+    //  Meu código ↑
     public static void main(String[] args) {
-        DesafioLamb p1 = new DesafioLamb("iPad", 3250.89, 0.13, "São José");
-        DesafioLamb p2 = new DesafioLamb("Computador", 8749.99, 0.15, "Palhoça");
-        DesafioLamb p3 = new DesafioLamb("Mouse", 350.00, 0.10, "Santo Amaro");
-        LojasEMuncipios(p1, p2, p3);
+        Function<Produto, Double> precoFinal = 
+            produto -> produto.preco * (1 - produto.desconto);
+
+        UnaryOperator<Double> impostoMunicipio = 
+            preco -> preco >= 2500 ? preco + 1.085 : preco ;
+
+        UnaryOperator<Double> frete =
+             preco -> preco >= 3000 ? preco + 100 : preco + 50;
+
+
+        Function<Double, String> formatar = 
+            preco -> ("R$ " + String.format("%.2f", preco)).replace(".", ",");
+
+        Produto p = new Produto("iPad", 3235.89, 0.50);
+
+        String preco = precoFinal.andThen(impostoMunicipio).andThen(frete).andThen(formatar).apply(p);
+
+        System.out.println("O preço final é " + preco);
     }
 }
 
 // 1. A partir do produto calcular o preco real (com desconto) ✔
-// 2. Imposto Municipal >= 2500 (8,5%) / < 2500 (Isento)
-// 3. Frete >= 3000 (100) / < 3000 (50)
-// 4. Arredondar: Deixar duas casas decimais
-// 5. Formatar: R$ 1234,56
+// 2. Imposto Municipal >= 2500 (8,5%) / < 2500 (Isento) ✔
+// 3. Frete >= 3000 (100) / < 3000 (50) ✔
+// 4. Arredondar: Deixar duas casas decimais ✔
+// 5. Formatar: R$ 1234,56 ✔
