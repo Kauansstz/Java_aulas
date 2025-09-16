fa0a7cbc2b08e159fd08a0e8484ff3b61339d467
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-09-16 14:56:43 -0300

    Match

---
+++ projetos/src/Streams/Match.java
@@ -0,0 +1,22 @@
+package Streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Match {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno("Ana", 7.1);
+        Aluno a2 = new Aluno("Bia", 7.1);
+        Aluno a3 = new Aluno("Pietro", 8.1);
+        Aluno a4 = new Aluno("Gabi", 10.0);
+
+        List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4);
+
+        Predicate<Aluno> aprovados = n -> n.nota >= 7;
+        System.out.println(alunos.stream().allMatch(aprovados));
+        System.out.println(alunos.stream().anyMatch(aprovados));
+        System.out.println(alunos.stream().noneMatch(aprovados.negate()));
+        
+    }
+}
